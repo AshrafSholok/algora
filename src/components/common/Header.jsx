584166b7efcce518606811bb956d94738ce9daa9
Ashraf <ashrafsholok450@gmail.com>
2025-07-24 15:25:22 +0300

    add tailwind material V2.1.10

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,14 @@
+
+
+function Header() {
+  return (
+    <header>
+      <a href="/">
+      <img src="" alt="logo-img" className="size-1/12"/>
+      </a>
+      <h1>Welcome to My App</h1>
+    </header>
+  )
+}
+
+export default Header
