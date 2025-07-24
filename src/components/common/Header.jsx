545b23fdf4464c0809ef84b92f0cffb0f4811da7
Ashraf <ashrafsholok450@gmail.com>
2025-07-24 21:59:18 +0300

    add header file contain the navBar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,118 @@
-
-
-function Header() {
+import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+ 
+function NavList() {
   return (
-    <header>
-      <a href="/">
-      <img src="" alt="logo-img" className="size-1/12"/>
-      </a>
-      <h1>Welcome to My App</h1>
-    </header>
-  )
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Services
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Projects
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Blog
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Contact Us
+        </a>
+      </Typography>
+    </ul>
+  );
 }
-
-export default Header
+ 
+export default function Header() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  const handleWindowResize = () =>
+    window.innerWidth >= 960 && setOpenNav(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+ 
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+ 
+  return (
+  <Navbar className="mx-auto max-w-screen-2xl px-24 py-1 rounded-none">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-6 cursor-pointer py-1.5"
+        >
+          <img src="../../src/assets/images/logo.png" alt="Logo" className="w-32" />
+        </Typography>
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          ) : (
+            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+          )}
+        </IconButton>
+      </div>
+      {/* Mobile Collapse Menu */}
+      {/* <div className="lg:hidden">
+        <Collapse open={openNav}>
+          <div className="bg-white p-4 mt-2 mx-2 ">
+            <NavList />
+          </div>
+        </Collapse>
+      </div> */}
+    </Navbar>
+  );
+}
